bcfde7853db5255d0465df190118ca013778ec33
BEEMIL <powerpoopoo@naver.com>
2024-04-23 11:04:00 +0900

    JenkinsFile 무중단 배포 생성

---
+++ deploy/develop/script/deploy_server.sh
@@ -0,0 +1,40 @@
+# (1)
+REPOSITORY=/home
+BUILD_PATH=$(ls -tr ${REPOSITORY}/build/lib/*.war | tail -1)
+WAR_NAME=$(basename $BUILD_PATH)
+echo "> build file: $WAR_NAME"
+
+# (2)
+echo "> copy build file"
+DEPLOY_PATH=${REPOSITORY}/deploy/
+if [ ! -d $DEPLOY_PATH ]; then
+  mkdir $DEPLOY_PATH
+fi
+
+cp $BUILD_PATH $DEPLOY_PATH
+
+echo "> change file name"
+CP_WAR_PATH=$DEPLOY_PATH$WAR_NAME
+APPLICATION_WAR_NAME=application-deploy.war
+APPLICATION_WAR=$DEPLOY_PATH$APPLICATION_WAR_NAME
+
+echo "> create link"
+ln -Tfs $CP_WAR_PATH $APPLICATION_WAR
+
+# (3)
+echo "> Check application PID."
+CURRENT_PID=$(pgrep -f -n $APPLICATION_WAR_NAME)
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ];
+  then
+    echo "> No running applications found."
+else
+	echo "> kill -9 $CURRENT_PID"
+	kill -9 $CURRENT_PID
+	sleep 10
+fi
+
+# (4)
+echo "> Run application."
+nohup java -war -Dspring.profiles.active=local $APPLICATION_WAR > /dev/null 2> /dev/null < /dev/null &
