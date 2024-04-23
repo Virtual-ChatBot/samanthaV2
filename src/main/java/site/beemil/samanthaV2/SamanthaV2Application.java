bae1642498fe30703d450915e5b2b83eba1103c5
BEEMIL <powerpoopoo@naver.com>
2024-04-23 09:31:07 +0900

    Application 파일 수정

--- src/main/java/site/beemil/samanthaV2/SamanthaV2Application.java
+++ src/main/java/site/beemil/samanthaV2/SamanthaV2Application.java
@@ -2,9 +2,16 @@ package site.beemil.samanthaV2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SamanthaV2Application {
+public class SamanthaV2Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SamanthaV2Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SamanthaV2Application.class, args);
