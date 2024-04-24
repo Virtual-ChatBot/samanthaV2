4a4de97e008e653d06db0ec40bd3416985cb802a
BEEMIL <powerpoopoo@naver.com>
2024-04-24 20:25:01 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy_server.sh
+++ deploy/develop/script/deploy_server.sh
@@ -1,40 +1,72 @@
-# (1)
 REPOSITORY=/home/samantha/tomcat_data
-BUILD_PATH=$(ls -tr ${REPOSITORY}/prod1/*.war | tail -1)
-WAR_NAME=$(basename $BUILD_PATH)
-echo "> build file: $WAR_NAME"
 
-# (2)
-echo "> copy build file"
-DEPLOY_PATH=${REPOSITORY}/prod1/
-if [ ! -d $DEPLOY_PATH ]; then
-  mkdir $DEPLOY_PATH
+# Prod1 작업
+# (1.1)
+BUILD_PATH_PROD1=$(ls -tr ${REPOSITORY}/prod1/*.war | tail -1)
+WAR_NAME_PROD1=$(basename $BUILD_PATH_PROD1)
+echo "> build file for prod1: $WAR_NAME_PROD1"
+
+# (1.2)
+echo "> copy build file for prod1"
+DEPLOY_PATH_PROD1=${REPOSITORY}/prod1/
+if [ ! -d $DEPLOY_PATH_PROD1 ]; then
+  mkdir $DEPLOY_PATH_PROD1
 fi
+cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
 
-cp $BUILD_PATH $DEPLOY_PATH
+echo "> change file name for prod1"
+CP_WAR_PATH_PROD1=$DEPLOY_PATH_PROD1$WAR_NAME_PROD1
+APPLICATION_WAR_NAME_PROD1=ROOT.war
+APPLICATION_WAR_PROD1=$DEPLOY_PATH_PROD1$APPLICATION_WAR_NAME_PROD1
 
-echo "> change file name"
-CP_WAR_PATH=$DEPLOY_PATH$WAR_NAME
-APPLICATION_WAR_NAME=ROOT.war
-APPLICATION_WAR=$DEPLOY_PATH$APPLICATION_WAR_NAME
+echo "> create link for prod1"
+ln -Tfs $CP_WAR_PATH_PROD1 $APPLICATION_WAR_PROD1
 
-echo "> create link"
-ln -Tfs $CP_WAR_PATH $APPLICATION_WAR
+# (1.3)
+echo "> Check application PID for prod1."
+CURRENT_PID_PROD1=$(pgrep -f -n $APPLICATION_WAR_NAME_PROD1)
+echo "$CURRENT_PID_PROD1"
 
-# (3)
-echo "> Check application PID."
-CURRENT_PID=$(pgrep -f -n $APPLICATION_WAR_NAME)
-echo "$CURRENT_PID"
-
-if [ -z $CURRENT_PID ];
-  then
-    echo "> No running applications found."
+if [ -z $CURRENT_PID_PROD1 ]; then
+    echo "> No running applications found for prod1."
 else
-	echo "> kill -9 $CURRENT_PID"
-	kill -9 $CURRENT_PID
-	sleep 10
+    echo "> Restarting Docker container for prod1"
+    docker restart prod1
+    sleep 10
 fi
 
-# (4)
-echo "> Run application."
-nohup java -war -Dspring.profiles.active=prod $APPLICATION_WAR > /dev/null 2> /dev/null < /dev/null &
+
+# Prod2 작업
+# (2.1)
+BUILD_PATH_PROD2=$(ls -tr ${REPOSITORY}/prod2/*.war | tail -1)
+WAR_NAME_PROD2=$(basename $BUILD_PATH_PROD2)
+echo "> build file for prod2: $WAR_NAME_PROD2"
+
+# (1.2)
+echo "> copy build file for prod2"
+DEPLOY_PATH_PROD2=${REPOSITORY}/prod2/
+if [ ! -d $DEPLOY_PATH_PROD2 ]; then
+  mkdir $DEPLOY_PATH_PROD2
+fi
+cp $BUILD_PATH_PROD2 $DEPLOY_PATH_PROD2
+
+echo "> change file name for prod2"
+CP_WAR_PATH_PROD2=$DEPLOY_PATH_PROD2$WAR_NAME_PROD2
+APPLICATION_WAR_NAME_PROD2=ROOT.war
+APPLICATION_WAR_PROD2=$DEPLOY_PATH_PROD2$APPLICATION_WAR_NAME_PROD2
+
+echo "> create link for prod2"
+ln -Tfs $CP_WAR_PATH_PROD2 $APPLICATION_WAR_PROD2
+
+# (1.3)
+echo "> Check application PID for prod2."
+CURRENT_PID_PROD2=$(pgrep -f -n $APPLICATION_WAR_NAME_PROD2)
+echo "$CURRENT_PID_PROD2"
+
+if [ -z $CURRENT_PID_PROD2 ]; then
+    echo "> No running applications found for prod2."
+else
+    echo "> Restarting Docker container for prod2"
+    docker restart prod2
+    sleep 10
+fi
