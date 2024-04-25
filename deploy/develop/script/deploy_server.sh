cdd93deb4626ea0bfe347025c2ccab6acb9a0b5e
BEEMIL <powerpoopoo@naver.com>
2024-04-25 11:44:57 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy_server.sh
+++ deploy/develop/script/deploy_server.sh
@@ -30,7 +30,7 @@ echo "$CURRENT_PID_PROD1"
 if [ -z $CURRENT_PID_PROD1 ]; then
     echo "> Restarting Docker container for prod1"
     echo "Qkrdydclf12" | sudo -S docker restart prod1
-    sleep 15
+    sleep 20
 fi
 
 # Prod2 작업
@@ -61,6 +61,7 @@ CURRENT_PID_PROD2=$(pgrep -f -n $APPLICATION_WAR_NAME_PROD2)
 echo "$CURRENT_PID_PROD2"
 
 if [ -z $CURRENT_PID_PROD2 ]; then
+    sleep 20
     echo "> Restarting Docker container for prod2"
     echo "Qkrdydclf12" | sudo -S docker restart prod2
 fi
