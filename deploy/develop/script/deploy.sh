c188d4645a4411dd7e297b782ec7c63e02ebc084
BEEMIL <powerpoopoo@naver.com>
2024-04-26 02:36:08 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy.sh
+++ deploy/develop/script/deploy.sh
@@ -1,5 +1,5 @@
 #deploy.sh
-BASE_PATH=/home/samantha/tomcat_data
+HOME=/home/samantha/tomcat_data
 WAR_NAME=ROOT.war
 WAIT_TIME=10
 echo "> build 파일명: $WAR_NAME"
@@ -10,28 +10,14 @@ SERVER_IP=$2
 PROD1_PORT=$3
 PROD2_PORT=$4
 
+loop=1
+limitLoop=30
+
 #스크립트 내에서 변수 사용
 echo "Docker Password: $DOCKER_PASSWORD"
 echo "Server IP: $SERVER_IP"
 echo "Prod1 Port: $PROD1_PORT"
 echo "Prod2 Port: $PROD2_PORT"
-
-#private ip
-MY_IP=$(hostname -i)
-echo "내 아이피는 $MY_IP 입니다"
-
-loop=1
-limitLoop=30
-flag='false'
-
-
-if [ $MY_IP == $IP1 ]; then
-  OTHER_IP=$IP2
-elif [ $MY_IP == $IP2 ]; then
-  OTHER_IP=$IP1
-else
-  echo "> 일치하는 IP가 없습니다. "
-fi
 
 # Prod1 작업
 # (1.1)
@@ -47,10 +33,66 @@ if [ ! -d $DEPLOY_PATH_PROD1 ]; then
 fi
 cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
 #===================================생존 서버 확인=====================================
+echo "> 서버 체크 시작"
+for retry_count in {1..10};
+do
+  response=$(sudo curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
+  echo "> $retry_count : $response  : $up_count"
+  if [ $up_count -ge 1 ]; then
+    echo "> 서버 health 체크 성공"
+    break
+  fi
+  if [ $retry_count -eq 10 ]; then
+    echo "> 서버 health 체크 실패"
+    exit 1
+  fi
+  echo "> 실패 10초후 재시도"
+  sleep 10
+done
+
 #===================================프로세스 종료======================================
-echo "> Check application PID for prod1."
-CURRENT_PID_PROD1=$(pgrep -f -n $WAR_NAME)
-echo "$CURRENT_PID_PROD1"
+echo "> 구동중인 애플리케이션 pid 확인"
+IDLE_PID=(`ps -ef | grep  $WAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+ if [ ${#IDLE_PID[@]} = 0 ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  flag='true'
+else
+  for pid in "${IDLE_PID[@]}"
+  do
+      echo "> [$pid] gracefully shutdown"
+      kill -15 $pid
+  done
+  while [ $loop -le $limitLoop ]
+  do
+      PID_LIST=(`ps -ef | grep  $JAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+      if [ ${#PID_LIST[@]} = 0 ]
+      then
+          echo "> gracefully shutdown success "
+          flag='true'
+          break
+      else
+          for pid in "${PID_LIST[@]}"
+          do
+              echo "> [$loop/$limitLoop] $pid 프로세스 종료를 기다리는중입니다."
+          done
+          loop=$(( $loop + 1 ))
+          sleep 1
+          continue
+      fi
+  done
+fi
+if [ $flag == 'false' ];
+then
+    echo "> 프로세스 강제종료 시도"
+    sudo ps -ef | grep $WAR_NAME | grep -v 'grep' |  awk '{ print $2 }' | \
+    while read PID
+    do
+        echo "> [$PID] forced shutdown"
+        kill -9 $PID
+    done
+fi
 #=======================================배포===========================================
 if [ -z $CURRENT_PID_PROD1 ]; then
     echo "> Restarting Docker container for prod1"
