bec6fca6227738173b4dc7ebdc89dfce89eb93f4
BEEMIL <powerpoopoo@naver.com>
2024-04-26 02:00:44 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy_server.sh
+++ deploy/develop/script/deploy_server.sh
@@ -1,10 +1,9 @@
 #deploy_server.sh
-
 HOME=/home/samantha/tomcat_data
 WAR_NAME=ROOT.war
 WAIT_TIME=10
 
-# Jenkins 환경 변수에서 전달한 값들
+# Jenkins 환경 변수에서 전달한 값
 DOCKER_PASSWORD=$1
 SERVER_IP=$2
 PROD1_PORT=$3
@@ -29,24 +28,18 @@ if [ ! -d $DEPLOY_PATH_PROD1 ]; then
   mkdir $DEPLOY_PATH_PROD1
 fi
 cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
-
-echo "> change file name for prod1"
-CP_WAR_PATH_PROD1=$DEPLOY_PATH_PROD1$WAR_PATH_PROD1
-APPLICATION_WAR_PROD1=$DEPLOY_PATH_PROD1$WAR_NAME
-
-echo "> create link for prod1"
-ln -Tfs $CP_WAR_PATH_PROD1 $APPLICATION_WAR_PROD1
-
-# (1.3)
+#===================================생존 서버 확인=====================================
+#===================================프로세스 종료======================================
 echo "> Check application PID for prod1."
 CURRENT_PID_PROD1=$(pgrep -f -n $WAR_NAME)
 echo "$CURRENT_PID_PROD1"
-
+#=======================================배포===========================================
 if [ -z $CURRENT_PID_PROD1 ]; then
     echo "> Restarting Docker container for prod1"
     echo "$DOCKER_PASSWORD" | sudo -S docker restart prod1
     sleep $WAIT_TIME
 fi
+#===============================현재 서버 Health check=================================
 
 # Prod2 작업
 # (2.1)
@@ -61,21 +54,15 @@ if [ ! -d $DEPLOY_PATH_PROD2 ]; then
   mkdir $DEPLOY_PATH_PROD2
 fi
 cp $BUILD_PATH_PROD2 $DEPLOY_PATH_PROD2
-
-echo "> change file name for prod2"
-CP_WAR_PATH_PROD2=$DEPLOY_PATH_PROD2$WAR_PATH_PROD2
-APPLICATION_WAR_PROD2=$DEPLOY_PATH_PROD2$WAR_NAME
-
-echo "> create link for prod2"
-ln -Tfs $CP_WAR_PATH_PROD2 $APPLICATION_WAR_PROD2
-
-# (2.3)
+#===================================생존 서버 확인=====================================
+#===================================프로세스 종료======================================
 echo "> Check application PID for prod2."
 CURRENT_PID_PROD2=$(pgrep -f -n $WAR_NAME)
 echo "$CURRENT_PID_PROD2"
-
+#=======================================배포===========================================
 if [ -z $CURRENT_PID_PROD2 ]; then
     sleep $WAIT_TIME
     echo "> Restarting Docker container for prod2"
     echo "$DOCKER_PASSWORD" | sudo -S docker restart prod2
 fi
+#===============================현재 서버 Health check=================================
