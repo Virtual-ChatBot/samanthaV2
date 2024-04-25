3e58a327a55d1de8afe503edb2344c1e2eed1968
BEEMIL <powerpoopoo@naver.com>
2024-04-26 03:35:21 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy.sh
+++ deploy/develop/script/deploy.sh
@@ -1,7 +1,7 @@
 #deploy.sh
 HOME=/home/samantha/tomcat_data
 WAR_NAME=ROOT.war
-WAIT_TIME=10
+WAIT_TIME=3
 echo "> build 파일명: $WAR_NAME"
 
 #Jenkins 환경 변수에서 전달한 값
@@ -16,6 +16,7 @@ echo "Server IP: $SERVER_IP"
 echo "Prod1 Port: $PROD1_PORT"
 echo "Prod2 Port: $PROD2_PORT"
 
+#=======================================배포===========================================
 # Prod1 작업
 # (1.1)
 BUILD_PATH_PROD1=$(ls -tr $HOME/prod1/*.war | tail -1)
@@ -31,7 +32,7 @@ fi
 cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
 #===================================생존 서버 확인=====================================
 echo "> 서버 체크 시작"
-for retry_count in {1..10};
+for retry_count in {1..3};
 do
   response=$(curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health)
   up_count=$(echo $response | grep 'UP' | wc -l)
@@ -40,12 +41,12 @@ do
     echo "> 서버 health 체크 성공"
     break
   fi
-  if [ $retry_count -eq 10 ]; then
+  if [ $retry_count -eq 3 ]; then
     echo "> 서버 health 체크 실패"
     exit 1
   fi
-  echo "> 실패 10초후 재시도"
-  sleep 10
+  echo "> 실패 3초후 재시도"
+  sleep $WAIT_TIME
 done
 
 #===================================프로세스 종료======================================
@@ -63,7 +64,7 @@ else
   for pid in "${IDLE_PID[@]}"
   do
       echo "> [$pid] gracefully shutdown"
-      kill -15 $pid
+      echo $DOCKER_PASSWORD | sudo -S docker restart prod1
   done
   while [ $loop -le $limitLoop ]
   do
@@ -100,8 +101,6 @@ echo "> 파일명" $HOME/$WAR_NAME
 echo $DOCKER_PASSWORD | sudo -S docker restart prod1
 sleep $WAIT_TIME
 
-#===============================현재 서버 Health check=================================
-
 # Prod2 작업
 # (2.1)
 BUILD_PATH_PROD2=$(ls -tr $HOME/prod2/*.war | tail -1)
@@ -115,15 +114,80 @@ if [ ! -d $DEPLOY_PATH_PROD2 ]; then
   mkdir $DEPLOY_PATH_PROD2
 fi
 cp $BUILD_PATH_PROD2 $DEPLOY_PATH_PROD2
-#===================================생존 서버 확인=====================================
+
+echo "> 10초 후 Health check 시작"
+echo "> curl -s http://$SERVER_IP:$PROD2_PORT/actuator/health"
+#==================================현재 서버 확인======================================
+for retry_count in {1..3}; do
+  response=$(sudo curl -s http://$MY_IP:$DEPLOYED_PORT/actuator/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
+  if [ $up_count -ge 1 ]; then
+    echo "> Health check 성공"
+    break
+  else
+    echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+    echo "> Health check: ${response}"
+  fi
+
+  if [ $retry_count -eq 3 ]; then
+    echo "> Health check 실패. "
+    echo "> Nginx에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "> Health check 연결 실패. 재시도..."
+  sleep $WAIT_TIME
+done
+
 #===================================프로세스 종료======================================
-echo "> Check application PID for prod2."
-CURRENT_PID_PROD2=$(pgrep -f -n $WAR_NAME)
-echo "$CURRENT_PID_PROD2"
-#=======================================배포===========================================
-if [ -z $CURRENT_PID_PROD2 ]; then
-    sleep $WAIT_TIME
-    echo "> Restarting Docker container for prod2"
-    echo $DOCKER_PASSWORD | sudo -S docker restart prod2
+loop=1
+limitLoop=30
+flag='false'
+
+echo "> 구동중인 애플리케이션 pid 확인"
+IDLE_PID=(`ps -ef | grep  $WAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+ if [ ${#IDLE_PID[@]} = 0 ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  flag='true'
+else
+  for pid in "${IDLE_PID[@]}"
+  do
+      echo "> [$pid] gracefully shutdown"
+      echo $DOCKER_PASSWORD | sudo -S docker restart prod1
+  done
+  while [ $loop -le $limitLoop ]
+  do
+      PID_LIST=(`ps -ef | grep  $WAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+      if [ ${#PID_LIST[@]} = 0 ]
+      then
+          echo "> gracefully shutdown success "
+          flag='true'
+          break
+      else
+          for pid in "${PID_LIST[@]}"
+          do
+              echo "> [$loop/$limitLoop] $pid 프로세스 종료를 기다리는중입니다."
+          done
+          loop=$(( $loop + 1 ))
+          sleep 1
+          continue
+      fi
+  done
 fi
+if [ $flag == 'false' ];
+then
+    echo "> 프로세스 강제종료 시도"
+    sudo ps -ef | grep $WAR_NAME | grep -v 'grep' |  awk '{ print $2 }' | \
+    while read PID
+    do
+        echo "> [$PID] forced shutdown"
+        kill -9 $PID
+    done
+fi
+#=======================================배포===========================================
+echo "> 배포"
+echo "> 파일명" $HOME/$WAR_NAME
+echo $DOCKER_PASSWORD | sudo -S docker restart prod2
+sleep $WAIT_TIME
 #===============================현재 서버 Health check=================================
