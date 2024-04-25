684b2694abbc5241732cdf9ca1a857c882231514
BEEMIL <powerpoopoo@naver.com>
2024-04-25 12:13:38 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy_server.sh
+++ deploy/develop/script/deploy_server.sh
@@ -1,5 +1,5 @@
 REPOSITORY=/home/samantha/tomcat_data
-WAIT_TIME=15
+WAIT_TIME=20
 
 # Prod1 작업
 # (1.1)
@@ -31,7 +31,6 @@ echo "$CURRENT_PID_PROD1"
 if [ -z $CURRENT_PID_PROD1 ]; then
     echo "> Restarting Docker container for prod1"
     echo "Qkrdydclf12" | sudo -S docker restart prod1
-    sleep $WAIT_TIME
 fi
 
 # Prod2 작업
