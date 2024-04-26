0a85d5d5333a4aca91ce12ca89d6ede298d9ecc7
BEEMIL <powerpoopoo@naver.com>
2024-04-26 10:01:31 +0900

    블루 그린 무중단 배포 테스트

--- deploy/develop/script/deploy.sh
+++ deploy/develop/script/deploy.sh
@@ -1,7 +1,7 @@
 #deploy.sh
 HOME=/home/samantha/tomcat_data
 WAR_NAME=ROOT.war
-WAIT_TIME=15
+WAIT_TIME=10
 echo "> build 파일명: $WAR_NAME"
 
 #Jenkins 환경 변수에서 전달한 값
@@ -10,7 +10,6 @@ SERVER_IP=$2
 PROD1_PORT=$3
 PROD2_PORT=$4
 SERVER_LIST=$5
-SERVER_PORT=$6
 
 #스크립트 내에서 변수 사용
 echo "Docker Password: $DOCKER_PASSWORD"
@@ -18,13 +17,12 @@ echo "Server IP: $SERVER_IP"
 echo "Prod1 Port: $PROD1_PORT"
 echo "Prod2 Port: $PROD2_PORT"
 echo "Server Config: $SERVER_LIST"
-echo "Server Port: $SERVER_PORT"
 
 #===================================생존 서버 확인=====================================
-echo "> 5초 후 Health check 시작"
+echo "> 서버 체크 시작"
 echo "> curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health"
 
-for retry_count in {1..5};
+for retry_count in {1..3};
 do
   response=$(curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health)
   up_count=$(echo $response | grep 'UP' | wc -l)
@@ -33,11 +31,11 @@ do
     echo "> 서버 health 체크 성공"
     break
   fi
-  if [ $retry_count -eq 5 ]; then
+  if [ $retry_count -eq 3 ]; then
     echo "> 서버 health 체크 실패"
     exit 1
   fi
-  echo "> 실패 5초후 재시도"
+  echo "> 실패 10초후 재시도"
   sleep $WAIT_TIME
 done
 
@@ -46,64 +44,56 @@ loop=1
 limitLoop=30
 flag='false'
 
-echo "> 구동중인 애플리케이션 pid 확인"
-IDLE_PID=(`ps -ef | grep  $WAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
- if [ ${#IDLE_PID[@]} = 0 ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-  flag='true'
+echo "> 구동중인 애플리케이션 확인"
+response=$(curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health)
+up_count=$(echo $response | grep 'UP' | wc -l)
+  if [ $up_count -eq 0 ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 서버 종료하지 않습니다."
+    flag='true'
 else
-  for pid in "${IDLE_PID[@]}"
-  do
-      echo "> [$pid] gracefully shutdown"
-      echo $DOCKER_PASSWORD | sudo -S docker restart prod1
-  done
+  echo "> [$SERVER_LIST] 서버를 재시작합니다."
+  echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
   while [ $loop -le $limitLoop ]
   do
-      PID_LIST=(`ps -ef | grep  $WAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
-      if [ ${#PID_LIST[@]} = 0 ]
-      then
-          echo "> gracefully shutdown success "
-          flag='true'
-          break
-      else
-          for pid in "${PID_LIST[@]}"
-          do
-              echo "> [$loop/$limitLoop] $pid 프로세스 종료를 기다리는중입니다."
-          done
-          loop=$(( $loop + 1 ))
-          sleep 1
-          continue
-      fi
+    response=$(curl -s http://$SERVER_IP:$PROD1_PORT/actuator/health)
+    up_count=$(echo $response | grep 'UP' | wc -l)
+    if [ $up_count -eq 0 ]; then
+        echo "> 성공적으로 서버를 재시작하였습니다."
+        flag='true'
+        break
+    else
+        echo "> [$loop/$limitLoop] $SERVER_LIST 서버 종료를 기다리는중입니다."
+        loop=$(( $loop + 1 ))
+        sleep 1
+        continue
+    fi
   done
 fi
-if [ $flag == 'false' ];
-then
-    echo "> 프로세스 강제종료 시도"
-    sudo ps -ef | grep $WAR_NAME | grep -v 'grep' |  awk '{ print $2 }' | \
-    while read PID
-    do
-        echo "> [$PID] forced shutdown"
-        kill -9 $PID
-    done
+if [ $flag == 'false' ]; then
+    echo "> 서버 강제종료 시도"
+    if [ $loop -gt $limitLoop ]; then
+        echo "> 서버 종료를 기다리는 동안 시간이 초과되었습니다. 강제 종료를 시도합니다."
+        echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
+    else
+        echo "> [$SERVER_LIST] 프로세스 강제 종료합니다."
+        echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
+    fi
 fi
 
-#=======================================배포1===========================================
+#=======================================배포===========================================
 # Prod1 작업
 # (1.1)
-sleep $WAIT_TIME
-
-BUILD_PATH_PROD1=$(ls -tr $HOME/$SERVER_LIST/*.war | tail -1)
-WAR_PATH_PROD1=$(basename $BUILD_PATH_PROD1)
-echo "> build file for prod1: $WAR_PATH_PROD1"
-
-# (1.2)
-echo "> copy build file for prod1"
-DEPLOY_PATH_PROD1=$HOME/prod1
-if [ ! -d $DEPLOY_PATH_PROD1 ]; then
-  mkdir $DEPLOY_PATH_PROD1
-fi
-cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
+#BUILD_PATH_PROD1=$(ls -tr $HOME/$SERVER_LIST/*.war | tail -1)
+#WAR_PATH_PROD1=$(basename $BUILD_PATH_PROD1)
+#echo "> build file for prod1: $WAR_PATH_PROD1"
+#
+## (1.2)
+#echo "> copy build file for prod1"
+#DEPLOY_PATH_PROD1=$HOME/prod1
+#if [ ! -d $DEPLOY_PATH_PROD1 ]; then
+#  mkdir $DEPLOY_PATH_PROD1
+#fi
+#cp $BUILD_PATH_PROD1 $DEPLOY_PATH_PROD1
 
 #echo "> 배포"
 #echo "> 파일명" $HOME/$WAR_NAME
