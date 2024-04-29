365a98e82415dbc1ec3de2923134eb456cb7f05c
BEEMIL <powerpoopoo@naver.com>
2024-04-30 06:45:53 +0900

    무중단 배포 수정

--- deploy/develop/script/deploy.sh
+++ deploy/develop/script/deploy.sh
@@ -16,56 +16,64 @@ echo "Server IP: $SERVER_IP"
 echo "Prod Port: $PROD_PORT"
 echo "Server List: $SERVER_LIST"
 
-# 서버 health 체크 함수
-check_server_health() {
-    local retry_count=0
-    while [ $retry_count -lt $MAX_RETRY ]; do
-        response=$(curl -s --max-time 10 http://$SERVER_IP:$PROD_PORT/actuator/health)
-        up_count=$(echo $response | grep -o 'UP' | wc -l)
-        echo "> Retry: $((retry_count+1)), Response: $response, UP Count: $up_count"
-
-        if ! [[ "$up_count" =~ ^[0-9]+$ ]]; then
-            echo "> 서버 health 체크 오류: UP Count가 숫자가 아닙니다."
-            ((retry_count++))
-            continue
-        fi
-
-        if [ $up_count -ge 1 ]; then
-            echo "> 서버 health 체크 성공"
-            return 0
-        fi
-        echo "> 실패. 재시도 대기 중..."
-        sleep $WAIT_TIME
-        ((retry_count++))
-    done
-    echo "> 서버 health 체크 실패"
-    return 1
-}
-
 #===================================생존 서버 확인=====================================
 echo "> 서버 체크 시작"
-check_server_health || exit 1
+echo "> curl -s http://$SERVER_IP:$PROD_PORT/actuator/health"
+
+for retry_count in {1..3};
+do
+  response=$(curl -s http://$SERVER_IP:$PROD_PORT/actuator/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
+  echo "> $retry_count : $response  : $up_count"
+  if [ $up_count -ge 1 ]; then
+    echo "> 서버 health 체크 성공"
+    break
+  fi
+  if [ $retry_count -eq 3 ]; then
+    echo "> 서버 health 체크 실패"
+    exit 1
+  fi
+  echo "> 실패 10초후 재시도"
+  sleep $WAIT_TIME
+done
 
 #===================================프로세스 종료======================================
+loop=1
+limitLoop=10
+flag='false'
+
 echo "> 구동중인 애플리케이션 확인"
-if [ $(curl -s --max-time 10 http://$SERVER_IP:$PROD_PORT/actuator/health | grep 'UP' | wc -l) -eq 0 ]; then
+response=$(curl -s http://$SERVER_IP:$PROD_PORT/actuator/health)
+up_count=$(echo $response | grep 'UP' | wc -l)
+  if [ $up_count -eq 0 ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 서버 종료하지 않습니다."
+    flag='true'
 else
-    echo "> [$SERVER_LIST] 서버를 재시작합니다."
-    echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
-    loop=1
-    while [ $loop -le $WAIT_TIME ]; do
-        if check_server_health; then
-            echo "> 성공적으로 서버를 재시작하였습니다."
-            break
-        else
-            echo "> [$loop/$WAIT_TIME] $SERVER_LIST 서버 종료를 기다리는 중입니다."
-            sleep 1
-            ((loop++))
-        fi
-    done
-    if [ $loop -gt $WAIT_TIME ]; then
+  echo "> [$SERVER_LIST] 서버를 재시작합니다."
+  echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
+  while [ $loop -le $limitLoop ]
+  do
+    response=$(curl -s http://$SERVER_IP:$PROD_PORT/actuator/health)
+    up_count=$(echo $response | grep 'UP' | wc -l)
+    if [ $up_count -eq 1 ]; then
+        echo "> 성공적으로 서버를 재시작하였습니다."
+        flag='true'
+        break
+    else
+        echo "> [$loop/$limitLoop] $SERVER_LIST 서버 종료를 기다리는중입니다."
+        loop=$(( $loop + 1 ))
+        sleep 1
+        continue
+    fi
+  done
+fi
+if [ $flag == 'false' ]; then
+    echo "> 서버 강제종료 시도"
+    if [ $loop -gt $limitLoop ]; then
         echo "> 서버 종료를 기다리는 동안 시간이 초과되었습니다. 강제 종료를 시도합니다."
+        echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
+    else
+        echo "> [$SERVER_LIST] 프로세스 강제 종료합니다."
         echo $DOCKER_PASSWORD | sudo -S docker restart $SERVER_LIST
     fi
 fi
